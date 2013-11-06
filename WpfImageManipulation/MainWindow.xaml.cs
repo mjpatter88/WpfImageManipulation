33739857fc6ca9eceeb39a27cfa2a120148708e7
Michael Patterson <mjpatter88@gmail.com>
2013-11-06 01:48:29 -0600

    Added some buttons to the GUI and added functionality to support Black and White filters. Initial testing suggests that it's working!

--- WpfImageManipulation/MainWindow.xaml.cs
+++ WpfImageManipulation/MainWindow.xaml.cs
@@ -31,13 +31,14 @@ namespace WpfImageManipulation
         // There seem to be several ways to do this, but the clearest explanation I've found starts on page 287 of "WPF Control Development Unleashed" by Pavan Podila and Kevin Hoffman.
 
         private BitmapImage image;
+        private WriteableBitmap filteredImage;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Load_Click_1(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openWindow = new OpenFileDialog();
             openWindow.Title = "Select an image to load.";
@@ -90,7 +91,7 @@ namespace WpfImageManipulation
         {
             #if DEBUG
                 ComboBoxItem selected = (ComboBoxItem)e.AddedItems[0];
-                MessageBox.Show("Combo option box selection: " + selected.Content.ToString());
+                Console.WriteLine("Combo option box selection: " + selected.Content.ToString());
             #endif
             
             switch(this.FilterSelect.SelectedIndex)
@@ -112,10 +113,75 @@ namespace WpfImageManipulation
                     Console.WriteLine("Case 3 entered.");
                     break;
                 default:
-                    MessageBox.Show("This filter not supported. ");
+                    MessageBox.Show("This filter not supported.");
                     break;
             }
 
+        }
+
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            #if DEBUG
+                Console.WriteLine("Apply Filter Clicked with filter: " + this.FilterSelect.SelectedIndex);
+            #endif
+
+            if(this.FilterSelect.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must first select a filter.");
+            }
+            else
+            {
+                //First create a writeable Bitmap from the image.
+                //TODO: Maybe add this to background queue after image is initially loaded?
+                this.filteredImage = new WriteableBitmap(image);
+
+                switch (this.FilterSelect.SelectedIndex)
+                {
+                    case 0:
+                        //Black and White filter
+                        ApplyFilterBW();
+                        break;
+                    case 1:
+                        //Sobel Edge Detection filter
+                        break;
+                    case 2:
+                        //Other Edge Detection filter
+                        break;
+                    case 3:
+                        //Voroni Sections filter
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        //Apply the Black and White Filter
+        private void ApplyFilterBW()
+        {
+            int height = image.PixelHeight;
+            int width = image.PixelWidth;
+
+            int[] pixels = new int[width * height]; //To hold the new pixels
+
+            this.filteredImage.CopyPixels(pixels, 4 * width, 0);
+
+            //Perform filter on each pixel
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                //Calculate each of the color components
+                int red = (pixels[i] & 0x00FF0000) >> 16;;
+                int green = (pixels[i] & 0x0000FF00) >> 8;
+                int blue = pixels[i] & 0x000000FF;
+                int intensity = (int)((0.3 * red) + (0.59 * green) + (0.11 * blue));
+
+                pixels[i] = intensity << 16;    //Red
+                pixels[i] |= intensity << 8;    //Green
+                pixels[i] |= intensity;         //Blue
+            }
+
+            this.filteredImage.WritePixels(new Int32Rect(0, 0, width, height), pixels, 4 * width, 0);
+            imageDisplay.Source = this.filteredImage;
         }
     }
 }
